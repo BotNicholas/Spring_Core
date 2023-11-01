5eac43e614dcd5894f784004c99493d4ca59c2a5
BotNicholas <botannicolai22@gmail.com>
2023-11-01 20:57:42 +0200

    Realize setter based DI

--- src/main/java/com/nicholas/spring/MusicPlayer.java
+++ src/main/java/com/nicholas/spring/MusicPlayer.java
@@ -3,7 +3,14 @@ package com.nicholas.spring;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer(){}
+
     private MusicPlayer(Music music){
+        this.music = music;
+    }
+
+
+    public void setMusic(Music music){
         this.music = music;
     }
 
