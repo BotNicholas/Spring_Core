24bb428c284da7eba1586d49e1a8b4528b11df06
BotNicholas <botannicolai22@gmail.com>
2023-11-02 15:38:22 +0200

    Move project to Java code and Annotations based configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+import com.nicholas.spring.AppConfig;
 import com.nicholas.spring.MusicGenre;
 import com.nicholas.spring.MusicPlayer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
 
